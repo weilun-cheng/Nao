91c244795ad0a6ff994aaa8483cbca58c4835177
cola <chengweilun@outlook.com>
2016-08-30 00:40:05 +0800

    add code

--- detection.py
+++ detection.py
@@ -6,8 +6,12 @@ import cv2
 import numpy as np
 from naoqi import ALProxy
 import math
+
 IP="169.254.76.111"
 PORT=9559
+
+low=2
+up=5
 
 def getImage(IP, PORT, flag):
     camProxy = ALProxy("ALVideoDevice", IP, PORT)
@@ -36,48 +40,8 @@ def getImage(IP, PORT, flag):
     return ci
 
 def check(IP, PORT, flag):
-    detection = 0
-    counter=0
+    detection=0
     frame = getImage(IP, PORT, flag)
-    frame = cv2.pyrMeanShiftFiltering(np.asarray(frame), 10.0, 35.0)
-    frame = cv2.blur(frame, (5, 5))
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    lower_green = np.array([35, 43, 46])
-    upper_green = np.array([77, 255, 255])
-    lower_red = np.array([0, 43, 46])
-    upper_red = np.array([10, 255, 255])
-    lower_red2 = np.array([156, 43, 46])
-    upper_red2 = np.array([180, 255, 255])
-    Rmask1 = cv2.inRange(hsv, lower_red, upper_red)
-    Rmask2 = cv2.inRange(hsv, lower_red2, upper_red2)
-    Gres = cv2.inRange(hsv, lower_green, upper_green)
-    Rres = cv2.bitwise_or(Rmask1, Rmask2)
-    res = cv2.bitwise_or(Gres, Rres)
-    ret, binary = cv2.threshold(res, 127, 255, cv2.THRESH_BINARY)
-    contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    for i in range(len(contours)):
-        a = cv2.contourArea(contours[i], False)
-        b = cv2.arcLength(contours[i], False)
-    	c = cv2.moments(contours[i])
-    	# if b != 0 and 200 < a < 76800:
-     #    	d = c['mu11'] / (c['m00'] * c['m00'])
-     #    	print b * b / a, a, c['nu20'], c['nu02']
-     #    	if flag == 0 and (10 < b * b / a < 35) and (3000 > a > 200) and (0.02 < c['nu20'] < 0.05) and (0.20 < c['nu02'] < 0.37):
-     #    		detection = 1
-     #    		break
-     #    	if flag==1 and (10 < b * b / a < 35) and (10000 > a > 1000) and (c['nu02'] / c['nu20'] > 4):
-     #    		detection=1
-     #    		break
-     	if cv2.contourArea(contours[i])>counter:
-	 		counter=cv2.contourArea(contours[i])
-	 		detection=1
-	 		point=i
-    return detection
-
-def GetCenter(IP, PORT, flag):
-    detection = 0
-    frame = getImage(IP, PORT, flag)
-    frame = cv2.pyrMeanShiftFiltering(np.asarray(frame), 10.0, 35.0)
     # frame = cv2.blur(frame, (5, 5))
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     lower_green = np.array([35, 43, 46])
@@ -94,46 +58,62 @@ def GetCenter(IP, PORT, flag):
     res= cv2.medianBlur(res,5) 
     ret, binary = cv2.threshold(res, 127, 255, cv2.THRESH_BINARY)
     contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    point=0
-    counter=0
     for i in range(len(contours)):
-    #     a = cv2.contourArea(contours[i], False)
-    #     b = cv2.arcLength(contours[i], False)
-    #     c = cv2.moments(contours[i])
-    # 	# if b != 0 and 200 < a < 76800:
-    #     d = c['mu11'] / (c['m00'] * c['m00'])
-    #     print b * b / a, a, c['nu20'], c['nu02']
-    #     if flag == 0 and (10 < b * b / a < 35) and (3000 > a > 200) and (0.02 < c['nu20'] < 0.05) and (0.20 < c['nu02'] < 0.37):
-    #     	point=i
-    #     	break
-    #     if flag==1 and (10 < b * b / a < 35) and (10000 > a > 1000) and (c['nu02'] / c['nu20'] > 4):
-    #     	point=i
-    #     	break
-    # 	if cv2.contourArea(contours[i])>counter:
-	 		# counter=cv2.contourArea(contours[i])
-	 		# point=i
-  		d=cv2.minAreaRect(contours[i])
-  		if d[1][1]!=0.0 and d[1][0]!=0.0:
-                    if (d[1][0]/d[1][1])>5.4 and (d[1][0]/d[1][1])<6 and cv2.contourArea(contours[i])>(d[1][1]*d[1][0]*0.8):
-                        point=contours[i]
-                        detection=1
-    c = cv2.moments(contours[point])
-    y= c['m10'] / c['m00']
-    x = c['m01'] / c['m00']
-    # cv2.drawContours(frame,contours[point],-1,(0,0,255),3)
-    print (x,y)
-    # cv2.namedWindow('image')
-    # while(1):
-  		# cv2.imshow('image',frame)
-  		# if cv2.waitKey(20)&0xFF==27:
-  		# 	break
-    # cv2.destoryAllWindows()
-    return (x, y)
+        area=cv2.contourArea(contours[i])
+        d=cv2.minAreaRect(contours[i])
+        if d[1][1]>d[1][0]:
+            a=d[1][1]
+            b=d[1][0]
+        else:
+            a=d[1][0]
+            b=d[1][1]
+        if a!=0.0 and b!=0.0:
+            if (a/b)>low and (a/b)<up and area>500:
+                detection=1
+                point=contours[i]
+    return detection
+
+def GetCenter(IP, PORT, flag):
+    detection = 0
+    x=y=0
+    frame = getImage(IP, PORT, flag)
+    # frame = cv2.blur(frame, (5, 5))
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    lower_green = np.array([35, 43, 46])
+    upper_green = np.array([77, 255, 255])
+    lower_red = np.array([0, 43, 46])
+    upper_red = np.array([10, 255, 255])
+    lower_red2 = np.array([156, 43, 46])
+    upper_red2 = np.array([180, 255, 255])
+    mask1 = cv2.inRange(hsv, lower_red, upper_red)
+    mask2 = cv2.inRange(hsv, lower_red2, upper_red2)
+    mask3 = cv2.inRange(hsv, lower_green, upper_green)
+    res = cv2.bitwise_or(mask1, mask2)
+    res = cv2.bitwise_or(res, mask3)
+    res= cv2.medianBlur(res,5) 
+    ret, binary = cv2.threshold(res, 127, 255, cv2.THRESH_BINARY)
+    contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    for i in range(len(contours)):
+        area=cv2.contourArea(contours[i])
+        d=cv2.minAreaRect(contours[i])
+        if d[1][1]>d[1][0]:
+            a=d[1][1]
+            b=d[1][0]
+        else:
+            a=d[1][0]
+            b=d[1][1]
+        if a!=0.0 and b!=0.0:
+            if (a/b)>low and (a/b)<up and area>500:
+                detection=1
+                point=contours[i]
+    if detection==1:
+        c=cv2.moments(point)
+        x=c['m10']/c['m00']
+        y=c['m01']/c['m00']
+    return (x,y)
 
 def GetArea(IP, PORT, flag):
-    detection = 0
     frame = getImage(IP, PORT, flag)
-    frame = cv2.pyrMeanShiftFiltering(np.asarray(frame), 10.0, 35.0)
     frame = cv2.blur(frame, (5, 5))
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     lower_green = np.array([35, 43, 46])
@@ -142,27 +122,29 @@ def GetArea(IP, PORT, flag):
     upper_red = np.array([10, 255, 255])
     lower_red2 = np.array([156, 43, 46])
     upper_red2 = np.array([180, 255, 255])
-    Rmask1 = cv2.inRange(hsv, lower_red, upper_red)
-    Rmask2 = cv2.inRange(hsv, lower_red2, upper_red2)
-    Gres = cv2.inRange(hsv, lower_green, upper_green)
-    Rres = cv2.bitwise_or(Rmask1, Rmask2)
-    res = cv2.bitwise_or(Gres, Rres)
+    mask1 = cv2.inRange(hsv, lower_red, upper_red)
+    mask2 = cv2.inRange(hsv, lower_red2, upper_red2)
+    mask3 = cv2.inRange(hsv, lower_green, upper_green)
+    res = cv2.bitwise_or(mask1, mask2)
+    res = cv2.bitwise_or(res, mask3)
+    res= cv2.medianBlur(res,5) 
     ret, binary = cv2.threshold(res, 127, 255, cv2.THRESH_BINARY)
     contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    area=0
     for i in range(len(contours)):
-        a = cv2.contourArea(contours[i], False)
-        b = cv2.arcLength(contours[i], False)
-    	c = cv2.moments(contours[i])
-    	if b != 0 and 200 < a < 76800:
-        	d = c['mu11'] / (c['m00'] * c['m00'])
-        	print b * b / a, a, c['nu20'], c['nu02']
-        	if flag == 0 and (10 < b * b / a < 35) and (3000 > a > 200) and (0.02 < c['nu20'] < 0.05) and (0.20 < c['nu02'] < 0.37):
-        		point=i
-        		break
-        	if flag==1 and (10 < b * b / a < 35) and (10000 > a > 1000) and (c['nu02'] / c['nu20'] > 4):
-        		point=i
-        		break
-    return cv2.countourArea(contours[point])
+        area=cv2.contourArea(contours[i])
+        d=cv2.minAreaRect(contours[i])
+        if d[1][1]>d[1][0]:
+            a=d[1][1]
+            b=d[1][0]
+        else:
+            a=d[1][0]
+            b=d[1][1]
+        if a!=0.0 and b!=0.0:
+            if (a/b)>low and (a/b)<up and area>500:
+                detection=1
+                point=contours[i]
+    return area
 
 def check2(IP, PORT, flag):
     detection = 0
@@ -172,16 +154,83 @@ def check2(IP, PORT, flag):
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     lower_black = np.array([0, 0, 0])
     upper_black = np.array([180, 255, 46])
-    mask = cv2.inRange(hsv, lower_black, upper_black)
-    res = cv2.bitwise_or(Gres, Rres)
+    res = cv2.inRange(hsv, lower_black, upper_black)
+    res= cv2.medianBlur(res,5) 
     ret, binary = cv2.threshold(res, 127, 255, cv2.THRESH_BINARY)
     contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     for i in range(len(contours)):
-  		d=cv2.minAreaRect(contours[i])
-  		if d[1][1]!=0.0 and d[1][0]!=0.0:
-                    if (d[1][1]/d[1][0])>1.4 and (d[1][1]/d[1][0])<1.6 and cv2.contourArea(contours[i])>(d[1][1]*d[1][0]*0.8):
-                        point=contours[i]
-                        detection=1
+        area=cv2.contourArea(contours[i])
+        d=cv2.minAreaRect(contours[i])
+        if d[1][1]>d[1][0]:
+            a=d[1][1]
+            b=d[1][0]
+        else:
+            a=d[1][0]
+            b=d[1][1]
+        if a!=0.0 and b!=0.0:
+            if (a/b)>1.4 and (a/b)<1.6 and area>(a*b*0.8):
+                point=contours[i]
+                detection=1
     return detection
 
-# center=GetCenter(IP,PORT,0)
+def GetCenter2(IP,PORT,flag):
+    detection = 0
+    frame = getImage(IP, PORT, flag)
+    frame = cv2.pyrMeanShiftFiltering(np.asarray(frame), 10.0, 35.0)
+    frame = cv2.blur(frame, (5, 5))
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    lower_black = np.array([0, 0, 0])
+    upper_black = np.array([180, 255, 46])
+    res = cv2.inRange(hsv, lower_black, upper_black)
+    res= cv2.medianBlur(res,5) 
+    ret, binary = cv2.threshold(res, 127, 255, cv2.THRESH_BINARY)
+    contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    x=y=0
+    for i in range(len(contours)):
+        area=cv2.contourArea(contours[i])
+        d=cv2.minAreaRect(contours[i])
+        if d[1][1]>d[1][0]:
+            a=d[1][1]
+            b=d[1][0]
+        else:
+            a=d[1][0]
+            b=d[1][1]
+        if a!=0.0 and b!=0.0:
+            if (a/b)>1.4 and (a/b)<1.6 and area>(a*b*0.8):
+                point=contours[i]
+                detection=1
+    if detection==1:
+        c=cv2.moments(point)
+        x=c['m10']/c['m00']
+        y=c['m01']/c['m00']
+    return (x,y)
+
+def GetArea2(IP,PORT,flag):
+    detection = 0
+    frame = getImage(IP, PORT, flag)
+    frame = cv2.pyrMeanShiftFiltering(np.asarray(frame), 10.0, 35.0)
+    frame = cv2.blur(frame, (5, 5))
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    lower_black = np.array([0, 0, 0])
+    upper_black = np.array([180, 255, 46])
+    res = cv2.inRange(hsv, lower_black, upper_black)
+    res= cv2.medianBlur(res,5) 
+    ret, binary = cv2.threshold(res, 127, 255, cv2.THRESH_BINARY)
+    contours, hierarchy = cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    area=0
+    for i in range(len(contours)):
+        area=cv2.contourArea(contours[i])
+        d=cv2.minAreaRect(contours[i])
+        if d[1][1]>d[1][0]:
+            a=d[1][1]
+            b=d[1][0]
+        else:
+            a=d[1][0]
+            b=d[1][1]
+        if a!=0.0 and b!=0.0:
+            if (a/b)>1.4 and (a/b)<1.6 and area>(a*b*0.8):
+                point=contours[i]
+                detection=1
+    if detection==1:
+        area=cv2.contourArea(point)
+    return area
--- main.py
+++ main.py
@@ -8,54 +8,75 @@ import detection
 import config
 import posinit
 from naoqi import ALProxy
+import grab
 
-##Xupper=
-##Xlower=
-##Yupper=
-##Ylower=
 IP = "169.254.76.111" # set your Ip adress here
 PORT = 9559
 
-high=detection.getImage(IP,PORT,0).shape[0]
-wide=detection.getImage(IP,PORT,0).shape[1]
+high=480
+wide=640
 
 def main(IP,PORT):
     posinit.posinit()
     tts = ALProxy("ALTextToSpeech", IP, PORT)
     tts.say("Hello!")
     posinit.posinit()
-    center=(0,0)
-    while (center[1] > (wide/2+10)) or (center[1] < (wide/2-10)):
-        if detection.check(IP,PORT,0)==1:
+    center=detection.GetCenter(IP,PORT,0)
+    while (center[0] > (wide/2+10)) or (center[0] < (wide/2-10)):
+        if center==(0,0):
             center=detection.GetCenter(IP,PORT,0)
-            print detection
-            tts.say("found the bar,check the position")
-##        else:
-##            posinit.posinit2(45,0)
-##            if detection.check(IP,PORT,0)==1 :
-##                center=detection.GetCenter(IP,PORT,0)
-##            else:
-##                posinit.posinit2(-45,0)
-##                if detection.check(IP,PORT,0)==1:
-##                    center=detection.GetCenter(IP,PORT,0)
-        posinit.posinit()
-        if center[1] > (wide/2+10):
-            moveto.move(0,3,0)
-            tts.say("found the bar,check the position")
-        elif center[1] < (wide/2-10):
-            moveto.move(0,-3,0)
-        else:
-            break
+            print center
+            continue
+        if center[0] > (wide/2+10):
+            moveto.move(0,-2,0)
+            tts.say("found the bar,check the x position")
+            print "found the bar,check the x position"
+        elif center[0] < (wide/2-10):
+            moveto.move(0,2,0)
+            tts.say("found the bar,check the x position")
+            print "found the bar,check the x position"
+        center=detection.GetCenter(IP,PORT,0)
+        print center
     center=detection.GetCenter(IP,PORT,0)
     print "Found the bar"
     tts.say("found the bar")
-   
-    moveto.move(30,0,0)
+    while center!=(0,0):
+        moveto.move(6,0.1,0)
+        center=detection.GetCenter(IP,PORT,0)
+        print center
+    center=detection.GetCenter(IP,PORT,0)
+    while center!=(0,0):
+        moveto.move(6,0.1,0)
+        center=detection.GetCenter(IP,PORT,0)
+    tts.say("change camera")
+    print "change camera"
+    
+
     posinit.posinit()
-    center=GetCenter(IP,PORT,1)
-##    while Xlower>center[0] and center[0]>Xupper and Ylower>center[1] and center[1]>Yupper:
-##        moveto.move((center[0]-wide/2)/50,(center[1]-high/2)/50,0)
-##    grab.grab(IP,PORT)
+    center=detection.GetCenter(IP,PORT,1)
+    while (center[0] > (wide/2+10)) or (center[0] < (wide/2-10)):
+        if center==(0,0):
+            center=detection.GetCenter(IP,PORT,1)
+            print center
+            continue
+        if center[0] > (wide/2+10):
+            moveto.move(0,-2,0)
+            tts.say("found the bar,check the position")
+            print "found the bar,check the x position"
+        elif center[0] < (wide/2-10):
+            moveto.move(0,2,0)
+            tts.say("found the bar,check the position")
+            print "found the bar,check the x position"
+        center=detection.GetCenter(IP,PORT,1)
+        print center
+    center=detection.GetCenter(IP,PORT,1)
+    while center[0]<320 or center[0]>340 or center[1]>285 or center[1]<220:
+        DeltaX=int((330-center[0])/15)
+        DeltaY=int((250-center[1])/20)
+        moveto.move(DeltaX,DeltaY,0)
+    print "ready to grab"
+    tts.say("ready to grab")
+    grab.grab(IP,PORT)
 
 if __name__ == '__main__':
     IP="169.254.76.111"
